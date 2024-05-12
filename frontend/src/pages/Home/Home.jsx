5aa93ec1e8866fd29bba8e6aaa70d55d037263bd
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-12 14:59:07 +0300

    Added Explore menu component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+
 import './Home.css';
 
 const Home = () => {
+    const [category,setCategory] = useState('All');
+
     return (
         <div>
             <Header />
+            <ExploreMenu category={category} setCategory={setCategory}/>
         </div>
     );
 };
