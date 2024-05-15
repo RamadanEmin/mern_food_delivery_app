b0408c0e10cf13a098ee2dcc4a9bb8668a8376cc
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-15 19:06:56 +0300

    Added Place order page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 
 import './Navbar.css';
 
 const Navbar = ({ setShowLogin }) => {
     const [menu, setMenu] = useState('home');
+
+    const { getTotalCartAmount } = useContext(StoreContext);
 
     return (
         <div className="navbar">
@@ -21,9 +24,7 @@ const Navbar = ({ setShowLogin }) => {
                 <img src={assets.search_icon} alt="" />
                 <div className="navbar-search-icon">
                     <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-                    <div className="dot">
-
-                    </div>
+                    <div className={getTotalCartAmount() === 0 ? '' : 'dot'}></div>
                 </div>
                 <button onClick={() => setShowLogin(true)}>
                     Sign In
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,54 @@
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
+
 import './PlaceOrder.css';
 
 const PlaceOrder = () => {
+    const { getTotalCartAmount } = useContext(StoreContext);
+
     return (
-        <div>PlaceOrder</div>
+        <form className='place-order'>
+            <div className="place-order-left">
+                <p className="title">Delivery Information</p>
+                <div className="multi-fields">
+                    <input required type="text" placeholder='First Name' />
+                    <input required type="text" placeholder='Last Name' />
+                </div>
+                <input className='email' required type="email" placeholder='Email address' />
+                <input className='street' required type="text" placeholder='Street' />
+                <div className="multi-fields">
+                    <input required type="text" placeholder='City' />
+                    <input required type="text" placeholder='State' />
+                </div>
+                <div className="multi-fields">
+                    <input required type="text" placeholder='Zip code' />
+                    <input required type="text" placeholder='Country' />
+                </div>
+                <input className='phone' required type="text" placeholder='Phone' />
+            </div>
+            <div className="place-order-right">
+                <div className="cart-total">
+                    <h2>Cart Totals</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Subtotal</p>
+                            <p>${getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delivery Fee</p>
+                            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <b>Total</b>
+                            <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+                        </div>
+                    </div>
+                    <button type='submit'>PROCEED TO PAYMENT</button>
+                </div>
+            </div>
+        </form>
     );
 };
 
