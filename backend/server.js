6d396e469c842f7d0d90d01bede0647025359fea
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-16 16:51:59 +0300

    Connected to Mongo DB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config'
+import { connectDB } from './config/db.js'
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 app.use(cors());
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('API Working');
