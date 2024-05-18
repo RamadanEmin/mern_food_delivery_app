496a7dc9859a83615760388bc30f9bf70df0d1b7
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-18 12:45:59 +0300

    Added Sidebar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
 
 const App = () => {
     return (
         <div>
             <Navbar />
             <hr />
+            <div className="app-content">
+                <Sidebar />
+            </div>
         </div>
     );
 };
