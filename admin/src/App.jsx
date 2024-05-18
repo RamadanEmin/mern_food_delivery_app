12ba7d1ba9d1c110f803562a29b98a052a6029d2
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-18 13:51:03 +0300

    Added Add page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,7 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
+import Add from './pages/Add/Add';
 
 const App = () => {
     return (
@@ -8,6 +10,9 @@ const App = () => {
             <hr />
             <div className="app-content">
                 <Sidebar />
+                <Routes>
+                    <Route path="/add" element={<Add />} />
+                </Routes>
             </div>
         </div>
     );
