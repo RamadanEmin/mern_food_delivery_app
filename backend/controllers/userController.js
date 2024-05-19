455d6c6aea312a6e4ca17cbe325b8996437c2472
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-19 21:28:36 +0300

    Added Register user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,40 @@
+import userModel from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
+
+const registerUser = async (req, res) => {
+    const { name, password, email } = req.body;
+
+    try {
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({ success: false, message: 'User already exists.' });
+        }
+
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: 'Please enter a valid email.' });
+        }
+
+
+        if (password.length < 8) {
+            return res.json({ success: false, message: 'Please enter a strong password.' });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name: name,
+            email: email,
+            password: hashedPassword
+        });
+
+        const user = await newUser.save();
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: 'Error' });
+    }
+};
+
+export { registerUser };
