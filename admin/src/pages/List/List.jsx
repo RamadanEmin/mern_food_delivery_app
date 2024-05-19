1659056bf697e0bfe6face1de40a4002031c9862
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-19 10:18:23 +0300

    Show list of foods

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -11,7 +11,6 @@ const List = ({ url }) => {
 
     const fetchList = async () => {
         const response = await axios.get(`${url}/api/food/list`);
-        console.log(response.data);
         if (response.data.success) {
             setList(response.data.data);
         }
@@ -24,7 +23,31 @@ const List = ({ url }) => {
         fetchList();
     }, []);
 
-    return;
+    return (
+        <div className='list add flex-col'>
+            <p>All Foods List</p>
+            <div className="list-table">
+                <div className="list-table-format title">
+                    <b>Image</b>
+                    <b>Name</b>
+                    <b>Category</b>
+                    <b>Price</b>
+                    <b>Action</b>
+                </div>
+                {list.map((item, index) => {
+                    return (
+                        <div key={index} className='list-table-format'>
+                            <img src={`${url}/images/` + item.image} alt="" />
+                            <p>{item.name}</p>
+                            <p>{item.category}</p>
+                            <p>${item.price}</p>
+                            <p className='cursor'>X</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default List;
