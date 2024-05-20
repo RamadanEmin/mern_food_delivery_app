c969c89c3a3caaa4785769a4a77faf8f651dbd1a
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-20 16:52:10 +0300

    Fetch List of foods from DB

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from 'react';
-import { food_list } from '../assets/assets';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState('');
+    const [food_list, setFoodList] = useState([]);
 
     const url = 'http://localhost:8000';
 
@@ -35,6 +36,22 @@ const StoreContextProvider = (props) => {
 
         return totalAmount;
     };
+
+    const fetchFoodList = async () => {
+        const response = await axios.get(url + '/api/food/list');
+        setFoodList(response.data.data);
+    };
+
+    useEffect(() => {
+        async function loadData() {
+            await fetchFoodList();
+            if (localStorage.getItem('token')) {
+                setToken(localStorage.getItem('token'));
+            }
+        }
+        
+        loadData();
+    }, []);
 
     const contextValue = {
         food_list,
