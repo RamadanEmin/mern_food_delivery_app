e4170d10ab4c5e8cfe341f1d33536bf0f9e28239
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-22 17:50:06 +0300

    Styled verify page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Verify from './pages/Verify/Verify';
 import Footer from './components/Footer/Footer';
 import LoginPopup from './components/LoginPopup/LoginPopup';
 
@@ -22,6 +23,7 @@ const App = () => {
                     <Route path='/' element={<Home />} />
                     <Route path='/cart' element={<Cart />} />
                     <Route path='/order' element={<PlaceOrder />} />
+                    <Route path='/verify' element={<Verify />} />
                 </Routes>
             </div>
             <Footer />
