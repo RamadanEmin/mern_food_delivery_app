24d98ec51cc1388c9775e5735bf4192d9b12cee5
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-22 17:35:50 +0300

    Added Verify payment page

---
+++ frontend/src/pages/Verify/Verify.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useContext } from 'react';
+import axios from 'axios';
+import { StoreContext } from '../../context/StoreContext';
+
+import './Verify.css';
+
+const Verify = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const success = searchParams.get('success');
+    const orderId = searchParams.get('orderId');
+    const { url } = useContext(StoreContext);
+    const navigate = useNavigate();
+
+    const verifyPayment = async () => {
+        const response = await axios.post(url + '/api/order/verify', { success, orderId });
+        if (response.data.success) {
+            navigate('/myorders');
+        }
+        else {
+            navigate('/');
+        }
+    };
+
+    useEffect(() => {
+        verifyPayment();
+    }, []);
+
+    return (
+        <div className='verify'>
+            <div className="spinner"></div>
+        </div>
+    );
+};
+
+export default Verify;
