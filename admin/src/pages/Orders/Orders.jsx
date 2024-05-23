6582a0ac7065994bb5e8cf431def5d3d49a76325
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-23 21:56:25 +0300

    Added Orders page

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import axios from 'axios';
+import { assets } from '../../assets/assets';
 
 import './Orders.css';
 
@@ -14,7 +15,7 @@ const Orders = ({ url }) => {
         if (response.data.success) {
             setOrders(response.data.data);
             console.log(response.data.data);
-        }else {
+        } else {
             toast.error('Error');
         }
     };
@@ -24,8 +25,40 @@ const Orders = ({ url }) => {
     }, []);
 
     return (
-        <div>
-            Order
+        <div className='order add'>
+            <h3>Order Page</h3>
+            <div className="order-list">
+                {orders.map((order, index) => (
+                    <div key={index} className='order-item'>
+                        <img src={assets.parcel_icon} alt="" />
+                        <div>
+                            <p className='order-item-food'>
+                                {order.items.map((item, index) => {
+                                    if (index === order.items.length - 1) {
+                                        return item.name + ' x ' + item.quantity;
+                                    }
+                                    else {
+                                        return item.name + ' x ' + item.quantity + ', ';
+                                    }
+                                })}
+                            </p>
+                            <p className="order-item-name">{order.address.firstName + ' ' + order.address.lastName}</p>
+                            <div className="order-item-address">
+                                <p>{order.address.street + ','}</p>
+                                <p>{order.address.city + ', ' + order.address.state + ', ' + order.address.country + ', ' + order.address.zipcode}</p>
+                            </div>
+                            <p className="order-item-phone">{order.address.phone}</p>
+                        </div>
+                        <p>Items : {order.items.length}</p>
+                        <p>${order.amount}</p>
+                        <select value={order.status}>
+                            <option value="Food Processing">Food Processing</option>
+                            <option value="Out for delivery">Out for delivery</option>
+                            <option value="Delivered">Delivered</option>
+                        </select>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
